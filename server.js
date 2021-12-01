fb80402749a9d0e6392d73d71c2c2386199870be
Marianne Nordlund <marianne.nordlund.syned21jon@tucsweden.se>
2021-12-01 02:59:12 +0100

    post request

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'pang')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/test', function(req, res) {
+    var name = req.body.namn;
+    fs.appendFile("test.txt", name, (err) => {
+        if(err) throw err;
+    });
+    //console.log(name);
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
